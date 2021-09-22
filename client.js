a85d8031d35f37fe6df856079ea958347408de03
Ralenac <t.ralenac@gmail.com>
2021-09-22 15:45:22 -0400

    Snake game

--- client.js
+++ client.js
@@ -14,7 +14,20 @@ const connect = function () {
   conn.on('connect', () => { 
     console.log("Successfully connected to game server" );
     conn.write(`Name: ${name}`) // how to send the information to the server
+    // setTimeout (() => {
+    //   conn.write("Move: up");
+    // }, 50)
+    // setTimeout (() => {
+    //   conn.write("Move: left");
+    // }, 50)
   })
+
+  //Supported Move Commands
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
   
   
   // interpret incoming data as text
