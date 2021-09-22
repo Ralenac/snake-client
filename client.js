d745a2791ab7ab1ce5ca6a0d5cad0bcbe5adcc60
Ralenac <t.ralenac@gmail.com>
2021-09-22 15:32:12 -0400

    Sanke game

--- client.js
+++ client.js
@@ -6,16 +6,19 @@ const connect = function () {
     host: '135.23.223.133',
     port: '50542'
   }, () => {
-    console.log('test')
+    // console.log('test')
   });
+
+  const name = 'ts'
   
-  conn.on('connect', () => {
-    console.log('connected')
+  conn.on('connect', () => { 
+    console.log("Successfully connected to game server" );
+    conn.write(`Name: ${name}`) // how to send the information to the server
   })
   
   
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.setEncoding("utf8"); // get the information from the server
   conn.on('data', () => {
     console.log('Hello')
   })
