e7449c565039ae5aee57953c9e89dbbfb43916b1
Ralenac <t.ralenac@gmail.com>
2021-09-22 16:17:53 -0400

    Snake game

--- play.js
+++ play.js
@@ -3,6 +3,31 @@ const {connect} = require("./client.js")
 
 connect;
 
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", (key) => { 
+    handleUserInput(key)}) //process.stdin.on('data', (key) => { //this code is from "Event Handling and User Input" exercise // process.stdout.write('.');
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = ((key) => { //callback function
+   // \u0003 maps to ctrl+c input
+   if (key === '\u0003') {
+    process.exit();
+    }
+})
+ 
+
+
+  
+
+
 // // establishes a connection with the game server
 // const connect = function () {
 //   const conn = net.createConnection({
